15b917762b8251f18537f8dad19ac551f53a5a7e
Achyut Dahal <achutdahal14@gmail.com>
2025-01-17 03:37:08 -0500

    post component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,19 @@
 
 import React from 'react';
 
+import NavBar from '@/components/Navbar/navbar';
+import Post from '@/components/homepage/post';
+
 const HomePage = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <><NavBar /><Post profilePic=""
+      userName="Test User"
+      postTitle="Test Title"
+      contentArea="Govinda dahal is the boss of this project and Hem Gautam is the big boss same like Salman Khan with that big stomach and I won 55$ today" /></>
+
+    
   );
-}
+};
 
 export default HomePage;
+
